72e60a4670dbb9d1ff9ab438d0f2356791ae143e
Fang Chen <lori.chenfang@gmail.com>
2024-11-04 09:54:25 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+// add static pages
+
+/**
+ * name：链接的名称,
+ * file: 文件地址,
+ * description: 地址描述（悬浮链接展示）
+ */
+
+const pages = [
+    { name: "EBP Progress", file: "./source files/EBP_Progress.html", description: 'progress at the family and species level' },
+    { name: "EBP Progress_all taxa", file: "./source files/EBP_StackedRainbow_v3.html", description: 'animation display of progress at all taxa levels' },
+    { name: "Progress_PhyloTree_order", file: "./source files/PhyloTree_EBP_order.html", description: 'phylogenomic tree of all orders with at least one species sequenced by EBP affiliates' },
+    { name: "Affiliates Distribution", file: "./source files/projectsMap_v4.html", description: 'geographic and statistical distribution of EBP affiliates' },
+    { name: "Affiliates Progress", file: "./source files/Affiliates_Progress_sorted.html", description: 'progress of each EBP affiliate projects' },
+];
