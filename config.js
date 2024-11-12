5651def478b699c34f0ebfa72a2bf15b95669e29
Fang Chen <lori.chenfang@gmail.com>
2024-11-12 09:59:56 -0700

    updated changes to map

--- config.js
+++ config.js
@@ -10,6 +10,6 @@ const pages = [
     { name: "EBP Progress", file: "./source files/EBP_Progress.html", description: 'progress at the family and species level' },
     { name: "EBP Progress_all taxa", file: "./source files/EBP_StackedRainbow_v2.html", description: 'animation display of progress at all taxa levels' },
     { name: "Progress_PhyloTree_order", file: "./source files/PhyloTree_EBP_order.html", description: 'phylogenomic tree of all orders with at least one species sequenced by EBP affiliates' },
-    { name: "Affiliates Distribution", file: "./source files/projectsMap_v6.html", description: 'geographic and statistical distribution of EBP affiliates' },
+    { name: "Affiliates Distribution", file: "./source files/projectsMap_v7.html", description: 'geographic and statistical distribution of EBP affiliates' },
     { name: "Affiliates Progress", file: "./source files/Affiliates_Progress_sorted.html", description: 'progress of each EBP affiliate projects' },
 ];
