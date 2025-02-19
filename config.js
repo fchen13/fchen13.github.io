bd72361ffbaac8d9c39afee07fc94ed68b1bb6f0
Fang Chen <lori.chenfang@gmail.com>
2025-02-19 10:55:00 -0700

    updates on reordering the report

--- config.js
+++ config.js
@@ -9,8 +9,8 @@
 const pages = [
     { name: "EBP Assembly Progress_Family_Species", file: "./source files/EBP_Progress.html", description: 'assembly progress at the family and species level' },
     { name: "EBP Assembly Progress_Taxonomic_Level", file: "./source files/EBP_StackedRainbow_v2.html", description: 'animation display of assembly progress at the main taxonomic ranks' },
-    { name: "Affiliates Assembly Progress", file: "./source files/Affiliates_Progress_sorted.html", description: 'assembly progress across EBP affiliate projects' },
     { name: "EBP Assembly Progress_PhyloTree_Order", file: "./source files/PhyloTree_EBP_order.html", description: 'phylogenomic tree of all orders with at least one species sequenced by EBP affiliates' },
+    { name: "Affiliates Assembly Progress", file: "./source files/Affiliates_Progress_sorted.html", description: 'assembly progress across EBP affiliate projects' },    
     { name: "EBP Affiliate Map", file: "./source files/projectsMap_v4.html", description: 'geographic and statistical distribution of EBP affiliates' }, 
     { name: "EBP Network Map", file: "./source files/WireMaps_v2.html", description: 'wired map of EBP network and affiliate network' },
 ];
